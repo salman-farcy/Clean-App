0c00968cf52be4527140c4976fccd38d7fac2232
slaman-farcy <salmanfarcy253@gmail.com>
2024-09-11 15:18:38 +0600

    #private-route/create private Route

--- clean-service-client/src/routes/PrivateRoute.jsx
+++ clean-service-client/src/routes/PrivateRoute.jsx
@@ -1,1 +1,22 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
+const PrivateRoute = ({ children }) => {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-[calc(100vh-64px)] z-20">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    );
+  }
+
+  if (!isLoading && !user?.email) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
