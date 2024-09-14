a0f31f74ea739c74d3118732c6c3ee13c9b8fad4
slaman-farcy <salmanfarcy253@gmail.com>
2024-09-14 09:49:20 +0600

    mongoDB add in backend

--- clean-service-backend/index.js
+++ clean-service-backend/index.js
@@ -1,50 +1,50 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = 5001
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 5001;
+
+
+//Parsers
+app.use(express.json())
 
 //DB URI
-const uri = "mongodb+srv://CleaneApp:6XxUXiM51DuJ7Thy@cluster0.rb5g6hh.mongodb.net/Clean-App?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://CleaneApp:6XxUXiM51DuJ7Thy@cluster0.rb5g6hh.mongodb.net/Clean-App?retryWrites=true&w=majority&appName=Cluster0";
 //MongoDB Cennection
 const client = new MongoClient(uri, {
-   serverApi: {
-     version: ServerApiVersion.v1,
-     strict: true,
-     deprecationErrors: true,
-   }
- });
- 
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
- async function run() {
-   try {
-     // Connect the client to the server	(optional starting in v4.7)
-     await client.connect();
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-     const ServicesCollection = client.db('Clean-App').collection('Services')
-
-
-     app.get('/Api/v1/services',async (req, res) => {
-      const cursor = ServicesCollection.find()
-      const result = await cursor.toArray()
-       res.send(result)
-    })
-     // Send a ping to confirm a successful connection
-     await client.db("admin").command({ ping: 1 });
-     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-   } finally {
-     // Ensures that the client will close when you finish/error
-   //   await client.close();
-   }
- }
- run().catch(console.dir);
+    
 
 
-app.get('/', (req, res) => {
-  res.send('Hello Clean App User')
-})
 
 
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Clean App User");
+});
 
 app.listen(port, () => {
-  console.log(`Clean App Server is listening on prot ${port}`)
-})
+  console.log(`Clean App Server is listening on prot ${port}`);
+});
